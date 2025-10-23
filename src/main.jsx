2fe580664ce4fddd8905765dfe248625b75c22d0
JihyeKim <jihyekim830@gmail.com>
2025-10-23 15:30:32 +0900

    feat: React-Router-Dom을 사용하여 라우팅 구성

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import App from '@/App.jsx';
+import MovieDetail from '@/components/MovieDetail';
+import '@/index.css';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/details" element={<MovieDetail />} />
+    </Routes>
+  </BrowserRouter>,
+);
