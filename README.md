ef3f42e43fb2e17ce932484fad37deef60bdc72e
JihyeKim <jihyekim830@gmail.com>
2025-10-23 12:15:55 +0900

    init: 프로젝트 초기화 및 기본 설정

---
+++ README.md
@@ -0,0 +1,9 @@
+# Mini Project - OZ무비
+
+영화 정보를 조회할 수 있는 웹 애플리케이션입니다.
+
+## 기술 스택
+
+- React
+- React Router
+- Tailwind CSS
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Text from '@/components/Text';
+
+function App() {
+  return (
+    <>
+      <Text />
+      <A />
+      <p>Click on the Vite and React logos to learn more</p>
+    </>
+  );
+}
+
+export default App;
