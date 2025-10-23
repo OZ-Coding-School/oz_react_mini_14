fc3bf510e947f2433d074677b8c27143cf134e06
kimsowon <sowon836@gmail.com>
2025-10-23 22:49:22 +0900

    폴더 구조 정리

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -2,8 +2,8 @@ export default function MovieCard({ data }) {
   return (
     <div className="movieCard">
       <img
-        src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
-        alt={`${data.title}`}
+        src={`https://image.tmdb.org/t/p/w500${data.poster_path}`}
+        alt={data.title}
       />
       {data.title}
       {data.vote_average}
