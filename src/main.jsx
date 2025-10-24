a7dd97248b740df07822123cf90f33791e8bf95c
pje8740 <pje8740@cu.ac.kr>
2025-10-24 17:03:25 +0900

    feat: Nav바 표시

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MovieDetail from "./component/MovieDetail.jsx";
+import Layout from "./component/Layout.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/details" element={<MovieDetail />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App />} />
+          <Route path="/details" element={<MovieDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
