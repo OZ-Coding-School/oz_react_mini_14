489b6f9571641f1fe1e7e6151ae9df80bc35a515
JihyeKim <jihyekim830@gmail.com>
2025-10-27 19:04:42 +0900

    design: MovieCard 스타일 변경

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,13 +4,15 @@ import { Link } from 'react-router-dom';
 function MovieCard({ movie }) {
   return (
     <Link to={`/details/${movie.id}`}>
-      <article className="shrink-0 grow-0 overflow-hidden rounded-md border border-stone-300 bg-stone-50 shadow-sm">
+      <article className="mx-auto w-50 shrink-0 grow-0 overflow-hidden rounded-md border border-stone-300 bg-stone-50 shadow-sm">
         <img
           src={`${IMAGE_BASE_URL}${movie.poster_path}`}
           alt={movie.title}
           className="mb-2 h-[300px] w-full bg-stone-200 object-contain"
         />
-        <p className="mb-2 ml-2 text-sm font-bold">{movie.title}</p>
+        <p className="mx-2 mb-2 overflow-hidden text-sm font-bold overflow-ellipsis whitespace-nowrap">
+          {movie.title}
+        </p>
         <p className="mb-2 ml-2 text-xs text-stone-600">
           평점: {movie.vote_average}
         </p>
