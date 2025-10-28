6a25f3d6c64eaeb6764106bf7cc70585ba56839a
anneedesign-ui <anneedesign@gmail.com>
2025-10-28 17:05:05 +0900

    fix: 상세보기 메뉴 제거하여 404 에러 수정

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,23 +4,13 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <nav className="navbar">
-      <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
-          🎬 Movie App
-        </Link>
-        <ul className="navbar-menu">
-          <li className="navbar-item">
-            <Link to="/" className="navbar-link">
-              홈
-            </Link>
-          </li>
-          <li className="navbar-item">
-            <Link to="/details" className="navbar-link">
-              상세보기
-            </Link>
-          </li>
-        </ul>
-      </div>
+      <Link to="/" className="logo">
+        🎬 Movie App
+      </Link>
+
+      <Link to="/" className="nav-link">
+        홈
+      </Link>
     </nav>
   );
 }
