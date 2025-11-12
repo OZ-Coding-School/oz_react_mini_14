fd483732caf6437aa90188cf2beb4f472988f608
jwb <jeonwb622@gmail.com>
2025-11-12 14:26:12 +0900

    refactor: 절대경로 @types/node(path) 패키지 추가하여 수정

--- react-miniProject/vite.config.js
+++ react-miniProject/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,20 +15,20 @@ export default defineConfig({
     },
   },
   resolve: {
-    alias: [
-      { find: "@supabase_path", replacement: "/supabase" },
-      { find: "@", replacement: "/src" },
-      { find: "@apis", replacement: "/src/apis" },
-      { find: "@assets", replacement: "/src/assets" },
-      { find: "@components", replacement: "/src/components" },
-      { find: "@common", replacement: "/src/components/common" },
-      { find: "@constants", replacement: "/src/constants" },
-      { find: "@hooks", replacement: "/src/hooks" },
-      { find: "@lib", replacement: "/src/lib" },
-      { find: "@pages", replacement: "/src/pages" },
-      { find: "@store", replacement: "/src/store" },
-      { find: "@styles", replacement: "/src/styles" },
-      { find: "@utils", replacement: "/src/utils" },
-    ],
+    alias: {
+      "@supabase_path": path.resolve(__dirname, "./supabase"),
+      "@": path.resolve(__dirname, "./src"),
+      "@apis": path.resolve(__dirname, "./src/apis"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@common": path.resolve(__dirname, "./src/components/common"),
+      "@constants": path.resolve(__dirname, "./src/constants"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@lib": path.resolve(__dirname, "./src/lib"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@store": path.resolve(__dirname, "./src/store"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+    },
   },
 });
