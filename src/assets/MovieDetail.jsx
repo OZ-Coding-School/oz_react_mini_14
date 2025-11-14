0e667c2e7da54995b1e3c4a5d89c7100ae7551c7
박진우 <jamguda010@gmail.com>
2025-11-14 15:22:44 +0900

    A

--- src/assets/MovieDetail.jsx
+++ src/assets/MovieDetail.jsx
@@ -4,7 +4,7 @@ const MovieDetail = () => {
   return (
     <div className="max-w-5xl mx-auto p-6 grid grid-cols-3 gap-6">
       
-      {/* 왼쪽 : 포스터 */}
+     
       <div className="col-span-1">
         <img
           src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
@@ -13,10 +13,10 @@ const MovieDetail = () => {
         />
       </div>
 
-      {/* 오른쪽 : 제목 / 평점 / 장르 / 줄거리 */}
+     
       <div className="col-span-2 flex flex-col gap-4">
 
-        {/* 제목 + 평점 */}
+       
         <div className="flex gap-4">
           <div className="flex-1 bg-teal-800 text-white p-4 text-center rounded">
             <h2 className="text-xl font-bold">{movie.title}</h2>
@@ -26,12 +26,12 @@ const MovieDetail = () => {
           </div>
         </div>
 
-        {/* 장르 */}
+    
         <div className="bg-teal-800 text-white p-4 rounded text-center text-lg">
           {movie.genres.map((g) => g.name).join(" · ")}
         </div>
 
-        {/* 줄거리 */}
+        
         <div className="bg-teal-800 text-white p-6 rounded text-center">
           {movie.overview}
         </div>
