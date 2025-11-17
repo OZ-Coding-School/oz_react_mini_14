6c93425380979b679bc2705e63df4b7765312764
JihyeKim <jihyekim830@gmail.com>
2025-11-17 20:37:12 +0900

    refactor: movie API 함수 응답 처리 로직을 별도 함수로 분리

--- src/apis/movie.js
+++ src/apis/movie.js
@@ -1,24 +1,37 @@
 import { API_BASE_URL, API_END_POINTS } from '@/constants';
 
-async function getMovieList({ params }) {
-  const { page } = params;
-  const response = await fetch(
-    `${API_BASE_URL}${API_END_POINTS.POPULAR}?language=ko-KR&page=${page}`,
-    {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
-        accept: 'application/json',
-      },
-    },
-  );
+async function fetchMovieData({ fetchFn }) {
+  const response = await fetchFn();
 
   if (!response.ok)
-    throw Error(
-      `${response.status}: 데이터를 받아오지 못했습니다.\n잠시 후에 다시 시도해 주세요.`,
-    );
+    return {
+      data: null,
+      error: {
+        message: `${response.status}: 데이터를 받아오지 못했습니다.\n잠시 후에 다시 시도해 주세요.`,
+      },
+    };
 
   const data = await response.json();
+  return { data, error: null };
+}
+
+async function getMovieList({ params }) {
+  const { page } = params;
+  const { data, error } = await fetchMovieData({
+    fetchFn: () =>
+      fetch(
+        `${API_BASE_URL}${API_END_POINTS.POPULAR}?language=ko-KR&page=${page}`,
+        {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
+            accept: 'application/json',
+          },
+        },
+      ),
+  });
+
+  if (error) throw error;
   return {
     data: data.results,
     page: data.page,
@@ -28,47 +41,39 @@ async function getMovieList({ params }) {
 
 async function getMovieListByKeyword({ params }) {
   const { keyword } = params;
-  const response = await fetch(
-    `${API_BASE_URL}${API_END_POINTS.SEARCH}?query=${keyword}&language=ko-KR&page=1`,
-    {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
-        accept: 'application/json',
-      },
-    },
-  );
+  const { data, error } = await fetchMovieData({
+    fetchFn: () =>
+      fetch(
+        `${API_BASE_URL}${API_END_POINTS.SEARCH}?query=${keyword}&language=ko-KR&page=1`,
+        {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
+            accept: 'application/json',
+          },
+        },
+      ),
+  });
 
-  if (!response.ok) {
-    throw Error(
-      `${response.status}: 데이터를 받아오지 못했습니다.\n잠시 후에 다시 시도해 주세요.`,
-    );
-  }
-
-  const data = await response.json();
+  if (error) throw error;
   return data.results;
 }
 
 async function getMovieDetails({ params }) {
   const { id } = params;
-  const response = await fetch(
-    `${API_BASE_URL}${API_END_POINTS.DETAIL}/${id}?language=ko-KR`,
-    {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
-        accept: 'application/json',
-      },
-    },
-  );
+  const { data, error } = await fetchMovieData({
+    fetchFn: () =>
+      fetch(`${API_BASE_URL}${API_END_POINTS.DETAIL}/${id}?language=ko-KR`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
+          accept: 'application/json',
+        },
+      }),
+  });
 
-  if (!response.ok) {
-    throw Error(
-      `${response.status}: 데이터를 받아오지 못했습니다.\n잠시 후에 다시 시도해 주세요.`,
-    );
-  }
-
-  return await response.json();
+  if (error) throw error;
+  return data;
 }
 
 export { getMovieList, getMovieListByKeyword, getMovieDetails };
