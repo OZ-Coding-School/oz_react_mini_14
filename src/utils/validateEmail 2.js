7ca5094041cfb3c6e62411309de3c60c80248308
anneedesign-ui <anneedesign@gmail.com>
2025-11-18 15:00:39 +0900

    fix: lint 오류 및 코드리뷰 반영, NavBar 변수 정리

--- src/utils/validateEmail 2.js
+++
@@ -1,4 +0,0 @@
-// 이메일 형식 유효성 검사 함수(Email validation, 이메일 밸리데이션)
-export function validateEmail(email) {
-  return /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
-}
--- src/utils/validatePassword 2.js
+++
@@ -1,4 +0,0 @@
-// 패스워드 유효성 예시(minimum 6 letters, 최소6글자)
-export function validatePassword(password) {
-  return typeof password === "string" && password.length >= 6;
-}
