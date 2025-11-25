c47fdd24ee2944dc44c2430abe1371f0346598e8
JihyeKim <jihyekim830@gmail.com>
2025-11-25 16:20:11 +0900

    refactor: 에러 메시지를 상수로 분리

--- src/apis/movie.js
+++ src/apis/movie.js
@@ -1,4 +1,4 @@
-import { API_BASE_URL, API_END_POINTS } from '@/constants';
+import { API_BASE_URL, API_END_POINTS, ERROR_MESSAGE } from '@/constants';
 
 async function fetchMovieData({ fetchFn }) {
   const response = await fetchFn();
@@ -7,7 +7,7 @@ async function fetchMovieData({ fetchFn }) {
     return {
       data: null,
       error: {
-        message: `${response.status}: 데이터를 받아오지 못했습니다.\n잠시 후에 다시 시도해 주세요.`,
+        message: `${response.status}: ${ERROR_MESSAGE.FETCH_FAILED}`,
       },
     };
 
--- src/constants/index.js
+++ src/constants/index.js
@@ -2,3 +2,4 @@ export * from '@/constants/url';
 export * from '@/constants/validationRules';
 export * from '@/constants/auth';
 export * from '@/constants/ui';
+export * from '@/constants/errorMessage';
