d8cc26a91916f8cd14110bd5ebaffd3260669794
jwb <jeonwb622@gmail.com>
2025-11-30 16:10:25 +0900

    refactor: filter관련 로직 훅으로 이동

--- react-miniProject/src/hooks/useMovieCardList.js
+++ react-miniProject/src/hooks/useMovieCardList.js
@@ -1,37 +1,33 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { API_URL, API_KEY } from "@constants/api.js";
-// import movieApi from "../apis/movie.js";
+import { useSelector } from "react-redux";
 
 export default function useMovieCardList() {
-  const [movieList, setMovieList] = useState([]); //movieListDatas.results //누적된 영화 데이터
+  const [movieList, setMovieList] = useState([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
-  const [totalPages, setTotalPages] = useState(null); // API에서 주는 전체 페이지 수
+  const [totalPages, setTotalPages] = useState(null);
   const [error, setError] = useState(null);
+  const searchText = useSelector((state) => state.search.text);
 
   const fetchMovieInfo = useCallback(async (pageValue) => {
     try {
       setLoading(true);
-      setError(null); //error 추가
+      setError(null);
 
       const endPoint = `${API_URL}/popular?api_key=${API_KEY}&language=ko&page=${pageValue}`;
       const response = await fetch(endPoint);
 
       if (!response.ok) {
-        //데이터 사전 체크
         throw new Error(`API 응답 에러: ${response.status}`);
       }
 
       const jsonData = await response.json();
       const data = jsonData.results.filter((movie) => movie.adult === false);
-      // (jsonData.results || []).filter((movie) => movie.adult === false);
+
       if (typeof jsonData.total_pages === "number") {
         setTotalPages(jsonData.total_pages);
       }
-
-      // if (data.length === 0) {
-      //   throw new Error("영화 데이터를 찾을 수 없습니다.");
-      // }
 
       setMovieList((prev) => {
         const combined = [...prev, ...data];
@@ -54,12 +50,29 @@ export default function useMovieCardList() {
 
   const addMovie = useCallback(() => {
     setPage((prev) => {
-      if (totalPages && prev >= totalPages) return prev; // totalPages가 알려져 있고 이미 마지막이면 증가하지 않음
+      if (totalPages && prev >= totalPages) return prev;
       return prev + 1;
     });
   }, [totalPages]);
 
-  const hasMore = totalPages ? page < totalPages : true; // totalPages 없을 땐 기본적으로 true
+  const hasMore = totalPages ? page < totalPages : true;
 
-  return { movieList, addMovie, loading, page, hasMore, error };
+  const filteredMovies = useMemo(
+    () =>
+      movieList?.filter((movie) =>
+        movie.title.toLowerCase().includes(searchText.toLowerCase())
+      ),
+    [movieList, searchText]
+  );
+
+  return {
+    movieList,
+    addMovie,
+    loading,
+    page,
+    hasMore,
+    error,
+    filteredMovies,
+    searchText,
+  };
 }
